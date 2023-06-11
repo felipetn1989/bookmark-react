fea8283fc945ef6af4772fdcede618abc6fabc41
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-06-11 17:02:58 -0300

    Update FAQ.js

--- bookmark/src/components/blocks/FAQ.js
+++ bookmark/src/components/blocks/FAQ.js
@@ -3,6 +3,12 @@ import Button from "../buttons/Button";
 import arrow from "../images/icon-arrow.svg";
 
 export default function FAQ() {
+
+
+  function handleOnClick(e) {
+    console.log(e.target)
+  }
+
   return (
     <div className="grid gap-4 px-8 mt-[8.6875rem]">
       <h2 className="text-2xl font-medium px-2 tracking-wide">
@@ -13,25 +19,35 @@ export default function FAQ() {
         answered please feel free to email us.
       </p>
       <div className="mt-[1.9375rem] mb-7">
-        <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
-          <div className="flex items-center justify-between">
-            <h3 className="text-sm tracking-wider">What is Bookmark?</h3>
+        <div className="border-b py-[1.375rem] hover:cursor-pointer ">
+          <div
+            className="flex items-center justify-between"
+            onClick={handleOnClick}
+          >
+            <h3 className="text-sm tracking-wider hover:text-[#fa5757]">
+              What is Bookmark?
+            </h3>
             <img src={arrow} alt="Arrow icon" />
           </div>
-          <p className="hidden">
+
+          <p className="text-left text-[#9194a1] pt-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
             tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
             quis quam ornare mattis.
           </p>
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
-          <div className="flex items-center justify-between">
+          <div
+            className="flex items-center justify-between"
+            onClick={handleOnClick}
+          >
             <h3 className="text-sm tracking-wider">
               How can I request a new browser?
             </h3>
             <img src={arrow} alt="Arrow icon" />
           </div>
-          <p className="hidden">
+
+          <p className="text-left text-[#9194a1] pt-1">
             Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa,
             ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros
             aliquet convallis ultricies. Mauris augue massa, ultricies non
@@ -41,7 +57,10 @@ export default function FAQ() {
           </p>
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
-          <div className="flex items-center justify-between">
+          <div
+            className="flex items-center justify-between"
+            onClick={handleOnClick}
+          >
             <h3 className="text-sm tracking-wider">Is there a mobile app?</h3>
             <img src={arrow} alt="Arrow icon" />
           </div>
@@ -53,7 +72,11 @@ export default function FAQ() {
           </p>
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
-          <div className="flex items-center justify-between">
+          <div
+            id="4"
+            className="flex items-center justify-between"
+            onClick={handleOnClick}
+          >
             <h3 className="text-sm tracking-wider">
               What about other Chromium browsers?
             </h3>
