42f305f346f7834ebe3390bab6effedc51fa9534
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-06-11 17:15:35 -0300

    implemented accordion menu

--- bookmark/src/components/blocks/FAQ.js
+++ bookmark/src/components/blocks/FAQ.js
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Button from "../buttons/Button";
-
 import arrow from "../images/icon-arrow.svg";
 
 export default function FAQ() {
+  const [expandedItemId, setExpandedItemId] = useState(null);
 
-
-  function handleOnClick(e) {
-    console.log(e.target)
+  function handleOnClick(itemId) {
+    expandedItemId === itemId
+      ? setExpandedItemId(null)
+      : setExpandedItemId(itemId);
   }
 
   return (
@@ -22,71 +24,96 @@ export default function FAQ() {
         <div className="border-b py-[1.375rem] hover:cursor-pointer ">
           <div
             className="flex items-center justify-between"
-            onClick={handleOnClick}
+            onClick={() => handleOnClick(1)}
           >
             <h3 className="text-sm tracking-wider hover:text-[#fa5757]">
               What is Bookmark?
             </h3>
-            <img src={arrow} alt="Arrow icon" />
+            <img
+              src={arrow}
+              alt="Arrow icon"
+              className={expandedItemId === 1 ? "rotate-180" : ""}
+            />
           </div>
 
-          <p className="text-left text-[#9194a1] pt-1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
-            quis quam ornare mattis.
-          </p>
+          {expandedItemId === 1 && (
+            <p className="text-left text-[#9194a1] pt-1 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
+              quis quam ornare mattis.
+            </p>
+          )}
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
           <div
             className="flex items-center justify-between"
-            onClick={handleOnClick}
+            onClick={() => handleOnClick(2)}
           >
             <h3 className="text-sm tracking-wider">
               How can I request a new browser?
             </h3>
-            <img src={arrow} alt="Arrow icon" />
+            <img
+              src={arrow}
+              alt="Arrow icon"
+              className={expandedItemId === 2 ? "rotate-180" : ""}
+            />
           </div>
 
-          <p className="text-left text-[#9194a1] pt-1">
-            Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa,
-            ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros
-            aliquet convallis ultricies. Mauris augue massa, ultricies non
-            ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis
-            ultricies. Mauris augue massa, ultricies non ligula. Suspendisse
-            imperdiet.
-          </p>
+          {expandedItemId === 2 && (
+            <p className="text-left text-[#9194a1] pt-1 text-sm">
+              Vivamus luctus eros aliquet convallis ultricies. Mauris augue
+              massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus
+              eros aliquet convallis ultricies. Mauris augue massa, ultricies
+              non ligula. Suspendisse imperdie tVivamus luctus eros aliquet
+              convallis ultricies. Mauris augue massa, ultricies non ligula.
+              Suspendisse imperdiet.
+            </p>
+          )}
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
           <div
             className="flex items-center justify-between"
-            onClick={handleOnClick}
+            onClick={() => handleOnClick(3)}
           >
             <h3 className="text-sm tracking-wider">Is there a mobile app?</h3>
-            <img src={arrow} alt="Arrow icon" />
+            <img
+              src={arrow}
+              alt="Arrow icon"
+              className={expandedItemId === 3 ? "rotate-180" : ""}
+            />
           </div>
-          <p className="hidden">
-            Sed consectetur quam id neque fermentum accumsan. Praesent luctus
-            vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula
-            quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin
-            ex et ultricies bibendum.
-          </p>
+
+          {expandedItemId === 3 && (
+            <p className="text-left text-[#9194a1] pt-1 text-sm">
+              Sed consectetur quam id neque fermentum accumsan. Praesent luctus
+              vestibulum dolor, ut condimentum urna vulputate eget. Cras in
+              ligula quis est pharetra mattis sit amet pharetra purus. Sed
+              sollicitudin ex et ultricies bibendum.
+            </p>
+          )}
         </div>
         <div className="border-b py-[1.375rem] hover:cursor-pointer hover:text-[#fa5757]">
           <div
-            id="4"
             className="flex items-center justify-between"
-            onClick={handleOnClick}
+            onClick={() => handleOnClick(4)}
           >
             <h3 className="text-sm tracking-wider">
               What about other Chromium browsers?
             </h3>
-            <img src={arrow} alt="Arrow icon" />
+            <img
+              src={arrow}
+              alt="Arrow icon"
+              className={expandedItemId === 4 ? "rotate-180" : ""}
+            />
           </div>
-          <p className="hidden">
-            Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat
-            mi, at euismod dui. Aliquam vitae neque eget nisl gravida
-            pellentesque non ut velit.
-          </p>
+
+          {expandedItemId === 4 && (
+            <p className="text-left text-[#9194a1] pt-1 text-sm">
+              Integer condimentum ipsum id imperdiet finibus. Vivamus in
+              placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida
+              pellentesque non ut velit.
+            </p>
+          )}
         </div>
       </div>
       <Button
