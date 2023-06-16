48b976a6a5b538b2cca246eb56580a88ec8a2d71
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-06-16 09:56:10 -0300

    image adjustments

--- bookmark/src/components/blocks/Sharing.js
+++ bookmark/src/components/blocks/Sharing.js
@@ -2,13 +2,13 @@ import image from "../images/illustration-features-tab-3.svg";
 
 export default function Sharing() {
   return (
-    <div className="relative flex flex-col gap-20 my-[3.5625rem] lg:my-11 lg:gap-32 lg:flex-row lg:px-[8.5rem]">
+    <div className="relative flex flex-col gap-20 my-[3.5625rem] lg:my-11 lg:gap-32 lg:flex-row lg:max-w-[69rem] lg:m-auto">
       <img
-        className="m-auto lg:m-[unset]"
+        className="m-auto lg:m-[unset] lg:w-[130%] lg:max-w-[34rem]"
         src={image}
         alt="Drawing of a internet site home screen"
       />
-      <div className="absolute top-[2.1875rem] left-[-2rem] h-[12.75rem] w-[97%] bg-[#5368df] -z-10 rounded-r-full m-auto lg:w-[46.5%] lg:top-20 lg:h-[22rem]"></div>
+      <div className="absolute top-[2.1875rem] left-[-2rem] h-[12.75rem] w-[97%] bg-[#5368df] -z-10 rounded-r-full m-auto lg:w-[57.5%] lg:top-[5.25rem] lg:h-[22rem] lg:left-[-10.375rem]"></div>
       <div className="flex flex-col gap-3.5 lg:text-left lg:gap-7">
         <h3 className="text-2xl font-medium tracking-wide lg:text-[2rem] lg:mt-16">
           Share your bookmarks
